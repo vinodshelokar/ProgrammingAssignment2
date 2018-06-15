344cc91e614514dd7b7227fcc452c4045c92957d
vinodshelokar <39730232+vinodshelokar@users.noreply.github.com>
2018-06-15 13:59:20 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+#############################################################################
+## Following code combination of two functions creates inverse of the matrics 
+## Please note this is applicable only for invertable matrix
+#############################################################################
 
-## Write a short comment describing this function
+#################################################################
+## This function cache the inverse of the matrix
+makeCacheMatrix = function(matx = matrix()) {
+                  var1 = NULL
+                  set  = function(matx1) {
+                          matx <<- matx1
+                          var1 <<- NULL
+                  } 
+                  get         = function() matx
+                  setinverse  = function(inverse) var1  = inverse
+                  getinverse  = function() var1
+                  list(
+                  set         = set,
+                  get         = get,
+                  setinverse  = setinverse,
+                  getinverse  = getinverse)
+                  
+}
+##################################################################
 
-makeCacheMatrix <- function(x = matrix()) {
-
+##################################################################
+## This Function computes inverse of Matrix.
+cacheSolve      = function(matx, ...) {
+                  var1 = matx$getinverse()
+                  if (!is.null(var1)) {
+                      return(var1)
+                  }
+                  data = matx$get()
+                  var1 = solve(data, ...)
+                  matx$setinverse(var1)
+                  var1
 }
 
+B <- matrix(c(1,2,3,4),2,2)
+B1 <- makeCacheMatrix(B)
+cacheSolve(B1)
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+##################################################################
